64050967416e38ed787a76abf8cb402e76b35bca
Maciej <maciej.zelazo@protonmail.com>
2019-08-20 22:26:26 +0200

    added html structure of blog

---
+++ script.js
@@ -0,0 +1,23 @@
+$('.owl-main').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"],
+    autoplay: true,
+    items: 1
+})
+
+
+$('input').focus(function () {
+    $(this).parents('.form-group').addClass('focused');
+});
+
+$('input').blur(function () {
+    var inputValue = $(this).val();
+    if (inputValue == "") {
+        $(this).removeClass('filled');
+        $(this).parents('.form-group').removeClass('focused');
+    } else {
+        $(this).addClass('filled');
+    }
+})
